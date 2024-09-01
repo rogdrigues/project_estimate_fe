9435fedf10a63960d5b6f41ec9e7bd83ee758c5b
NguyenHungPhong <90857528+rogdrigues@users.noreply.github.com>
2024-09-01 13:32:54 +0700

    feat: Implemented automatic refresh token handling and session validation for secure access control

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/dashboard',
+                destination: '/auth/signin',
                 permanent: true,
             },
         ];
