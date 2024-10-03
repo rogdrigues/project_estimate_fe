35e8a19abf292013890cb2b15e7eb8b87affd9ac
NguyenHungPhong <90857528+rogdrigues@users.noreply.github.com>
2024-10-03 20:49:27 +0700

    feat: add checklist and resources project design

---
+++ src/app/(locale)/(user)/project/_component/tabs/checklists/project-checklists-selected-modal.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Checklist, ProjectChecklist } from '@/types';
+import SaveIcon from '@mui/icons-material/Save';
+import { Button, Box, Typography, Modal, Divider } from '@mui/material';
+import { updateProjectComponents } from '@/services';
+import { columns } from '@/app/(locale)/(user)/checklist/_table_config/checklist-table-columns';
+import { useToast } from '@/context/ToastContext';
+import TableComponentWithCheckbox from '@/components/_table_form-config/table-checkbox-component';
+import { HeaderButton } from '@/styles';
+
+interface IProps {
+    fetchSelectedChecklists: () => void;
+    projectId: string;
+    checklists: Checklist[];
+    components: ProjectChecklist[];
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+const ProjectChecklistsSelectedModal = (props: IProps) => {
+    const { projectId, checklists, open, setOpen, components, fetchSelectedChecklists } = props;
+    const { triggerToast } = useToast();
+    const [selectedChecklists, setSelectedChecklists] = useState<ProjectChecklist[]>([]);
+
+    const handleSave = async () => {
+        try {
+            const response = await updateProjectComponents(projectId, { checklists: selectedChecklists });
+            if (response.EC === 0) {
+                fetchSelectedChecklists();
+                triggerToast('The list of checklists has been updated to the project successfully.', true);
+                setOpen(false);
+            } else {
+                triggerToast(`Failed to update checklists: ${response.message}`, false);
+            }
+        } catch (error) {
+            triggerToast('Failed to update checklists.', false);
+        }
+    };
+
+    useEffect(() => {
+        if (components.length > 0) {
+            const selectedChecklistIds = components.map((component) => component.originalChecklistId);
+            setSelectedChecklists(selectedChecklistIds);
+        } else {
+            setSelectedChecklists([]);
+        }
+    }, [open, components, projectId]);
+
+    const handleRowsSelected = (selectedRows: string[]) => {
+        setSelectedChecklists(selectedRows);
+    };
+
+    return (
+        <Modal
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="project-checklists-modal"
+            aria-describedby="project-checklists-description"
+        >
+            <Box sx={{ width: '90%', height: '80%', margin: 'auto', marginTop: '5%', backgroundColor: 'white', padding: 4, borderRadius: 2, overflowY: 'auto' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2 }}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                        Project Checklists
+                    </Typography>
+                </Box>
+
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2, mt: -2 }}>
+                    <Typography variant="body2" sx={{ color: 'gray' }}>
+                        Select the checklists that apply to this project. After making your selection, click [Save] button to update the list of checklists into the project.
+                    </Typography>
+                    <Button
+                        variant="outlined"
+                        startIcon={<SaveIcon />}
+                        onClick={handleSave}
+                        color="primary"
+                        sx={HeaderButton}
+                    >
+                        Save
+                    </Button>
+                </Box>
+                <Divider sx={{ mb: 2 }} />
+                <TableComponentWithCheckbox<ProjectChecklist>
+                    rows={checklists}
+                    columns={columns}
+                    initialVisibility={{
+                        name: true,
+                        subClass: true,
+                        category: true,
+                        priority: true,
+                    }}
+                    hiddenColumnsOnMobile={['category']}
+                    onRowsSelected={handleRowsSelected}
+                    selectionRows={selectedChecklists}
+                />
+
+            </Box>
+        </Modal>
+    );
+}
+
+export default ProjectChecklistsSelectedModal;
---
+++ src/app/(locale)/(user)/project/_component/tabs/checklists/project-checklists.tsx
@@ -0,0 +1,99 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { Checklist, ProjectChecklist, Category } from '@/types';
+import HeaderComponent from '@/components/_table_form-config/header-item-component';
+import { getProjectComponents, getAllChecklists, getAllCategories } from '@/services'; // API gọi category
+import { useSession } from 'next-auth/react';
+import TableComponent from '@/components/_table_form-config/table-item-component';
+import { columns } from '@/app/(locale)/(user)/checklist/_table_config/checklist-table-columns';
+import ProjectChecklistsSelectedModal from '@/app/(locale)/(user)/project/_component/tabs/checklists/project-checklists-selected-modal';
+
+interface IProps {
+    projectId: string;
+}
+
+const ProjectChecklists = (props: IProps) => {
+    const { projectId } = props;
+    const [checklists, setChecklists] = useState<ProjectChecklist[]>([]);
+    const [originalChecklists, setOriginalChecklists] = useState<Checklist[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]); // Thêm state cho categories
+    const [open, setOpen] = useState(false);
+    const { data: session } = useSession();
+    const [openUpdate, setOpenUpdate] = useState(false);
+    const [dataView, setDataView] = useState<ProjectChecklist | null>(null);
+
+    const fetchSelectedChecklists = async () => {
+        try {
+            const checklistData = await getProjectComponents(projectId, 'checklists');
+            setChecklists(checklistData?.result || []);
+        } catch (error: any) {
+            console.error(`Error fetching checklists: ${error.message}`);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                fetchSelectedChecklists();
+
+                const originalChecklistData = await getAllChecklists(session?.access_token);
+                setOriginalChecklists(originalChecklistData?.result || []);
+
+                const categoryData = await getAllCategories(session?.access_token);
+                setCategories(categoryData?.result || []);
+            } catch (error: any) {
+                console.error(`Error fetching data: ${error.message}`);
+            }
+        };
+
+        fetchData();
+    }, [projectId]);
+
+    return (
+        <>
+            <HeaderComponent
+                title="Project Checklists"
+                onAssignOpen={() => setOpen(true)}
+                hideExportImport={true}
+                currentPage="project_detail"
+                showAssign={true}
+                modal={
+                    <ProjectChecklistsSelectedModal
+                        fetchSelectedChecklists={fetchSelectedChecklists}
+                        projectId={projectId}
+                        checklists={originalChecklists}
+                        components={checklists}
+                        open={open}
+                        setOpen={setOpen}
+                    />
+                }
+            />
+            <TableComponent
+                rows={checklists}
+                columns={columns}
+                onDelete={async () => { }}
+                onRestore={async () => { }}
+                markWord="checklist"
+                openUpdate={openUpdate}
+                setOpenUpdate={setOpenUpdate}
+                dataView={dataView}
+                setDataView={setDataView}
+                initialVisibility={{
+                    name: true,
+                    subClass: true,
+                    category: true,
+                    priority: true,
+                    status: true,
+                    actions: true,
+                }}
+                hiddenColumnsOnMobile={['description']}
+                currentPage="project_detail"
+            >
+                {/* Doing this later */}
+                Quac
+            </TableComponent>
+        </>
+    );
+};
+
+export default ProjectChecklists;
