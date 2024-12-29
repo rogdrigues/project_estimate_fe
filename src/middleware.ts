7d71dbed54c0cedfe8ea8938a351a3ad96f5e4a6
NguyenHungPhong <90857528+rogdrigues@users.noreply.github.com>
2024-12-29 13:06:16 +0700

    chore: remove vercel file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { string } from 'zod';
 import { customFetch } from './lib';
 import { UserMaster } from './types';
 export async function middleware(req: NextRequest) {
